2a15dee57ffad9eb22de674045b7899b75102f74
Jason Daou <jasondaou5@gmail.com>
2025-08-14 13:43:16 +0300

    Added Wallet frontend -Jason

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import './App.css';
-import { AuthenticationContainer } from './Containers/AuthenticationContainer';
-import { LoadingContainer } from './Containers/LoadingContainer';
-import { Full } from './Components/MainMenu/Full';
-import { ErrorContainer } from './Containers/ErrorContainer';
-import { Login } from './Forms/Login/Login';
-import { LoadingContext } from './ContextProvider/LoadingContext';
-import { mobileDevicesWidth, urlPath } from './globals';
-import PageError from './Containers/PageError';
+import React, { useContext, useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
+import { AuthenticationContainer } from "./Containers/AuthenticationContainer";
+import { LoadingContainer } from "./Containers/LoadingContainer";
+import { Full } from "./Components/MainMenu/Full";
+import { ErrorContainer } from "./Containers/ErrorContainer";
+import { Login } from "./Forms/Login/Login";
+import { LoadingContext } from "./ContextProvider/LoadingContext";
+import { mobileDevicesWidth, urlPath } from "./globals";
+import PageError from "./Containers/PageError";
 // import PDFViewer from './ReusableComponents/PdfViewer/PDFViewer';
+import Wallet from "./Components/Wallet/Wallet";
 
 function App() {
   const { setisLoading } = useContext(LoadingContext);
 
-
   const state = {
-    patientData: localStorage.getItem('patientData') ? JSON.parse(localStorage.getItem('patientData')) : {}
-  }
+    patientData: localStorage.getItem("patientData")
+      ? JSON.parse(localStorage.getItem("patientData"))
+      : {},
+  };
 
   return (
-
     // <PDFViewer />
 
     <Routes>
       <Route element={<ErrorContainer />}>
         <Route element={<LoadingContainer />}>
           <Route element={<AuthenticationContainer />}>
-            <Route path='/login' element={<Login />} />
-            <Route path='/error' element={<PageError />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/error" element={<PageError />} />
 
-            <Route path='/' element={<Full />}>
+            <Route path="/" element={<Full />}>
+              <Route path="/wallet" element={<Wallet />} />
             </Route>
           </Route>
         </Route>
-      </Route >
-    </Routes >
-
+      </Route>
+    </Routes>
   );
 }
 
